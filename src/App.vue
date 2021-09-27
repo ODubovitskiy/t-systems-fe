86539bef6919b4f41510af80781b3fa4f62ae59d
Oleg Dubovitskiy <oleg.dubovitskiy@gmail.com>
2021-09-27 12:45:01 +0300

    Created nav & side bar for truck view

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,35 @@
 <template>
   <div>
+    <div class="row">
+      <div class="col-md-12">
+        <navComponent/>
+      </div>
+    </div>
 
+    <div class="row">
+      <div class="col-md-2">
+        <sidebar/>
+      </div>
+      <div class="col-md-10">
+        <contentComponent/>
+      </div>
+    </div>
   </div>
 </template>
 
 
 <script>
+import navComponent from '@/components/layouts/nav-component'
+import sidebar from '@/components/layouts/sidebar'
+import contentComponent from '@/components/layouts/content-component'
 
 export default {
 
   name: 'App',
   components: {
-
+    contentComponent,
+    navComponent,
+    sidebar
 
   }
 }
